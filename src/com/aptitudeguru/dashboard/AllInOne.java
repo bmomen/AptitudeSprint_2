900fddee4f003698b8edb004a2db2bd85e7e8f81
bmomen <babakmomen@mail.com>
2015-03-17 22:34:17 +0000

    Start test not working
    Start test button not working, but list to select tests works!

--- src/com/aptitudeguru/dashboard/AllInOne.java
+++ src/com/aptitudeguru/dashboard/AllInOne.java
@@ -1,24 +1,144 @@
 package com.aptitudeguru.dashboard;
 
-
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CheckBox;
 
-import android.widget.TextView;
+import android.content.Context;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
 import androidhive.dashboard.R;
 
 public class AllInOne extends Activity {
+
+	int start = 1;
+	 final Context context = this;
+
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) { 
+	        // Activity was brought to front and not created, 
+	        // Thus finishing this will get us to the last viewed activity 
+	        finish(); 
+	        return; 
+	    } 
+		setContentView(R.layout.allinone);
+
+		
+		Button btn_home = (Button) findViewById(R.id.btn_home);
+
 	
+		Button btn_fav = (Button) findViewById(R.id.btn_fav);
+
+		
+		Button btn_score = (Button) findViewById(R.id.btn_score);
+
+		
+		Button btn_tutorial = (Button) findViewById(R.id.btn_soundon);
+
+		
+		Button btn_about = (Button) findViewById(R.id.btn_about);
+	Button btn_help = (Button) findViewById(R.id.btn_help);
+		
+		btn_help.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(),
+					Help.class);
+
+				startActivity(i);
+			}
+		});
+		
+
+		
+		btn_home.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(),
+						AndroidDashboardDesignActivity.class);
+				i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(i);
+			}
+		});
+
+		
+		btn_fav.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(), FavPage.class);
+
+				startActivity(i);
+			}
+		});
+
+		
+		btn_score.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(), scoremenu.class);
+				startActivity(i);
+			}
+		});
+
+	
+		btn_tutorial.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(),
+						Tutorialpage.class);
+				startActivity(i);
+			}
+		});
+
+		
+		btn_about.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+				Intent i = new Intent(getApplicationContext(), AboutUs.class);
+				startActivity(i);
+			}
+		});
+
+		
+		Button startbtn = (Button) findViewById(R.id.startbtn);
+
+		
+		startbtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				
+						String cat = "q1";
+						
+						Intent i = new Intent(getApplicationContext(), TestPageallinone.class);
+						i.putExtra("cat", cat);
+
+						startActivity(i);
+				
+
+			}
+		});
+
+
+	}
+
 }
